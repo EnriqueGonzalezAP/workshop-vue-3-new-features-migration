96f552052954a0e249b7c028280935f9fab3f96a
Enrique Gonzalez <Enrique.gonzalezalvarezpalencia@lumen.com>
2023-04-19 19:24:50 +0200

    Exercise 4

--- src/components/ModalOverlay.vue
+++ src/components/ModalOverlay.vue
@@ -1,3 +1,89 @@
 <template>
-  <p>Create your modal overlay component here</p>
+  <button class="button" @click="openModal">Open</button>
+  <teleport to="body">
+    <transition>
+      <div v-if="this.opened" class="background" @click="closeModal">
+        <div class="modal" @click="stopPropagation">
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+          <div class="modal-footer">
+            <button class="button" @click="closeModal">Close</button>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </teleport>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      opened: false
+    }
+  },
+  methods: {
+    openModal() {
+      this.opened = true
+    },
+    closeModal() {
+      this.opened = false
+    },
+    stopPropagation(e) {
+        e.stopPropagation();
+    }
+  }
+}
+</script>.
+
+<style scoped>
+  .background {
+    top: 0;
+    position: absolute;
+    height: 100vh;
+    width: 100%;
+    background: #000000ee;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    display: flex;
+    width: 500px;
+    height: fit-content;
+    flex-direction: column;
+    background: white;
+    color: black;
+    border-radius: 8px;
+  }
+  .modal-body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+  }
+  .modal-footer {
+    margin-top: 5px;
+    border-top: 1px solid black;
+    padding: 15px;
+  }
+  .button {
+    background: blue;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 8px;
+  }
+
+  /* we will explain what these classes do next! */
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+</style>
